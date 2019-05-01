1ef611c06c976847ea2c2d11f6ca93982d3503ae
nasimnoori <nasim.noori3@gmail.com>
2019-05-01 14:24:53 -0400

    body

--- web/dayforce.php
+++ web/dayforce.php
@@ -74,4 +74,4 @@ return json_encode($resp, JSON_PRETTY_PRINT);
 // $result = connect('https://www.dayforcehcm.com/api/sdi/v1/Employees/991465', ['IS_VALIDATE_ONLY' => true, 'EmployeeXRefCode' => 991465]);
 $result = connect('https://www.dayforcehcm.com/api/sdi/v1/Employees');
 
-var_dump($result);
+var_dump($result.body);

618d45d980aea3de5a330b296280f29f89321c8c
nasimnoori <nasim.noori3@gmail.com>
2019-05-01 11:47:36 -0400

    connection made

--- web/index.php
+++ web/index.php
@@ -1,8 +1,7 @@
 <?php
-$conn = pg_connect(getenv("postgres://izzkocpmsxkgwj:c6463f09ad3352a3879cdb6b8b274bcb2a632fd572c30ae7a50785cb92c02044@ec2-50-19-127-115.compute-1.amazonaws.com:5432/d9d3m5j1ppbsr7
-"));
+$conn = pg_connect(getenv("DATABASE_URL"));
 
-    var_dump($conn);
+var_dump($conn);
 
     echo "<h1>welcome to my first heroku App</h1>";
 ?>

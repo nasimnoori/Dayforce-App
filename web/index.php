1fc27a124d5f65585b52009470da988c99904d4f
nasimnoori <nasim.noori3@gmail.com>
2019-05-01 11:51:12 -0400

    list values

--- web/index.php
+++ web/index.php
@@ -3,5 +3,24 @@ $conn = pg_connect(getenv("DATABASE_URL"));
 
 var_dump($conn);
 
-    echo "<h1>welcome to my first heroku App</h1>";
+$query = 'SELECT * FROM Account';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($conn);
+
 ?>
